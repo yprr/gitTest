55e4008d446c1c9ef3fc31dccdc84adad9bea60c
yprr <yprr1001@163.com>
2022-10-15 19:09:45 +0800

    HelloWord

---
+++ gitTest/HelloWord.java
@@ -0,0 +1,15 @@
+
+package com.xup;
+
+/**
+ * @author xup
+ * @version 1.0.0
+ * @ClassName HelloWord.java
+ * @Description TODO
+ * @createTime 2022年10月15日 19:07:00
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("HelloWord");
+    }
+}
