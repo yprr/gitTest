a2c185bfb6f9d6c6ea842019097b96d7e4a1cf36
yprr <yprr1001@163.com>
2022-10-15 19:34:50 +0800

    "modify "

---
+++ src/main/java/com/xup/HelloWord.java
@@ -0,0 +1,20 @@
+package com.xup;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author xup
+ * @version 1.0.0
+ * @ClassName HelloWord.java
+ * @Description TODO
+ * @createTime 2022年10月15日 19:07:00
+ */
+public class HelloWord {
+
+    @Test
+    public void test1(){
+        System.out.println("HelloWord");
+    }
+
+
+}
